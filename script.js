1dd5e37e27a2d82f908780a8fb7a48e438abd847
f1my <floooomy@gmail.com>
2025-07-18 17:38:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-  const name = "JAQLIV";
+  const name = "Floomy";
   let nameText = '';
   let nameIndex = 0;
   let isNameDeleting = false;
@@ -236,7 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   const bioMessages = [
-    "Fu*k Guns.lol & Fakecrime.bio got banned too often, so I created my own.",
+    "Papa Floomy Bio",
     "\"Hello, World!\""
   ];
   let bioText = '';
