6302318cb3b3180fc89229f6d14abba2ff13bf3f
f1my <floooomy@gmail.com>
2025-07-22 19:40:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let hasUserInteracted = false;
-
 const audioFiles = [
   '14-system-music-daily-log-tv.ogg',
   '35-eshop-menu-track-10.ogg',
@@ -31,31 +29,7 @@ function playNextAudio() {
   currentAudioIndex++;
 }
 
-function initMedia() {
-  console.log("initMedia called");
-  const backgroundMusic = document.getElementById('background-music');
-  const backgroundVideo = document.getElementById('background');
-  if (!backgroundMusic || !backgroundVideo) {
-    console.error("Media elements not found");
-    return;
-  }
-  backgroundMusic.volume = 0.3;
-  backgroundVideo.muted = true;
-
-  // Initial shuffle, but do NOT play audio here, wait for user interaction
-  shuffledAudioFiles = shuffleArray([...audioFiles]);
-  backgroundMusic.src = shuffledAudioFiles[0]; // Set initial audio source
-  backgroundMusic.load(); // Pre-load the audio
-
-  backgroundMusic.addEventListener('ended', playNextAudio);
-
-  // backgroundVideo.play().catch(err => {
-  //   console.error("Failed to play background video:", err);
-  // });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  initMedia(); // Initialize media when the DOM is ready
   const startScreen = document.getElementById('start-screen');
   const startText = document.getElementById('start-text');
   const profileName = document.getElementById('profile-name');
@@ -185,16 +159,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // backgroundMusic.src = shuffledAudioFiles[0]; // Source is already set in initMedia
     
-    const playPromise = backgroundMusic.play();
-
-    if (playPromise !== undefined) {
-        playPromise.then(() => {
-            console.log('Audio playback started successfully!');
-            currentAudioIndex = 1;
-        }).catch(error => {
-            console.error('Failed to play audio:', error);
-        });
-    }
+    backgroundMusic.play().then(() => {
+        console.log('Audio playback started successfully!');
+        currentAudioIndex = 1;
+    }).catch(error => {
+        console.error('Failed to play audio:', error);
+    });
 
     profileBlock.classList.remove('hidden');
     gsap.fromTo(profileBlock,
